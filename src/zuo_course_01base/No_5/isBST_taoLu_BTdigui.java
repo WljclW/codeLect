c6e4adb3aff9af619a5f4610776a5ae808a07943
820173359@qq.com <820173359@qq.com>
2025-04-15 01:42:59 +0800

    递归逻辑

---
+++ src/zuo_course_01base/No_5/isBST_taoLu_BTdigui.java
@@ -0,0 +1,75 @@
+package zuo_course_01base.No_5;
+
+
+/**
+ *判断是不是二叉搜索树的递归版本。——————体会二叉树的递归套路
+ * */
+
+public class isBST_taoLu_BTdigui {
+    /**
+     * 对于任何一个节点，它的左子树、右子树分别返回三个信息：最大值、最小值、是否是搜索树。
+     * */
+    public static class ReturnData{
+        int min;
+        int max;
+        boolean isBST;
+        public ReturnData(int min,int max,boolean isBST){
+            this.min=min;
+            this.max=max;
+            this.isBST=isBST;
+        }
+    }
+
+    public static ReturnData process(Node x){
+        /**1. base case的处理*/
+        if(x == null){
+            return null;
+        }
+
+        /**2. 默认从左右子树拿到了想要的数据。。黑盒怎么实现不用管，我们只是假设就能拿
+         * 到ReturnData这种类型的数据，这就是我们需要的信息*/
+        ReturnData leftData = process(x.left);
+        ReturnData rightData = process(x.right);
+
+        /**3. 对于当前节点，利用已有信息计算需要返回的信息。。实现黑盒的拆解，拆解的完整内容就是计
+         * 算当前节点需要返回的信息*/
+        /**3.1 继续需要返回的min，max信息*/
+        int min = x.value;
+        int max = x.value;
+        if (leftData != null){  //如果leftData不为空，求左子树的最大值、最小值。。
+            min = Math.min(min,leftData.min);
+            max = Math.max(max, leftData.max);
+        }
+        if (rightData != null){
+            min = Math.min(min,rightData.min);
+            max = Math.max(max, rightData.max);
+        }
+        /**3.2 计算需要返回的isBST信息。即当前节点为根的子树 是不是平衡二叉树*/
+        boolean isBST = true; //先默认是true，然后判断是不是违规
+        if (leftData!=null && (!leftData.isBST || leftData.max >= x.value))
+        /**
+         *   !leftData.isBST为真时表明左子树不是搜索树，将isBST置为false。
+         *   leftData.max >= x.value表明父节点小于左子树某节点的值，将isBST置为false。
+         * */
+            isBST = false;
+        if (rightData!=null && (!rightData.isBST || x.value >= rightData.min))
+        /**
+         *   !rightData.isBST为真时表明左子树不是搜索树，将isBST置为false。
+         *   x.value >= rightData.min表明父节点大于右孩子某节点的值，将isBST置为false。
+         * */
+            isBST = false;
+
+        return new ReturnData(min,max,isBST);
+    }
+
+
+    public static void main(String[] args) {
+        Node n1=new Node(2);
+        Node n2=new Node(2);
+        Node n3=new Node(3);
+        n1.left=n2;
+        n1.right=n3;
+//        System.out.println(is_BST03(n1));
+        System.out.println(process(n1).isBST);
+    }
+}
--- src/zuo_course_01base/No_5/isBalanceBT.java
+++ src/zuo_course_01base/No_5/isBalanceBT.java
@@ -2,20 +2,21 @@ package zuo_course_01base.No_5;
 
 
 /**
- *   判断一棵树是否是平衡二叉树————递归思想
- *
+ * 判断一棵树是否是平衡二叉树————递归思想
  * */
 public class isBalanceBT {
     public static Boolean isBalance(Node head){
-        /**
-         *      这个方法充分的体现了二叉树的递归套路。
-         * */
         return process(head).isBalanced;
     }
-    public static class ReturnType{         //我们需要所有字数提供什么信息呢？？
+
+    /**
+     * 定义返回类型。是每一个节点为根的子树返回的数据的封装
+     * */
+    public static class ReturnType{         //我们需要所有子树提供什么信息呢？？
         /**
-         *      平衡二叉树这里左右子树需要提供两个信息：①是否平衡   ②高度。
-         *      且左右子树需要提供的是同样的两个信息————并集后依然是两个信息，因此返回类型就是包含二者的结合体。
+         *   平衡二叉树这里左右子树需要提供两个信息：①是否平衡   ②高度。
+         *   且左右子树需要提供的是同样的两个信息————并集后依然是两个信息，因此返回类
+         * 型就是包含二者的结合体。
          * */
         public boolean isBalanced;
         public int height;
@@ -26,17 +27,22 @@ public class isBalanceBT {
     }
 
     public static ReturnType process(Node x){
-        if (x == null)                                  //空节点是平衡的，并且高度是0
+        /**1. base case处理*/
+        if (x == null)      //空节点是平衡的，并且高度是0
             return new ReturnType(true,0);
 
-        //递归处理左右子树
+        /**2. 假设利用黑盒拿到了左右子树返回的，我们需要的数据*/
         ReturnType leftData = process(x.left);
         ReturnType rightData = process(x.right);
 
-        //需要把返回的2个信息如何得到进行说明
-        int height = Math.max(leftData.height,rightData.height)+1;          //高度就是左右子树高度的最大值+1,1表示x的高度。
+        /**3. 基于左右子树返回的结果，如何计算出当前节点应该返回的信息。。。即黑盒
+         * 逻辑的实现，最终实现闭环*/
+        /**3.1 根据已有信息，计算当前节点为根的子树的高度————即返回值的其中一个信息*/
+        int height = Math.max(leftData.height,rightData.height)+1;  //高度就是左右子树高度的最大值+1,1表示x的高度。
+        /**3.2 根据已有的信息，计算当前的节点是不是平衡的————这是返回值的第二个信息*/
         boolean isBalanced = leftData.isBalanced && rightData.isBalanced
-                            && Math.abs(leftData.height-rightData.height)<2;        //平衡的判断标准————左右子树都平衡并且左右子树高度差小于2
+                            && Math.abs(leftData.height-rightData.height)<2;   //平衡的判断标准————左右子树都平衡并且左右子树高度差小于2
+        /**3.3 将两个信息封装，并返回*/
         return new ReturnType(isBalanced,height);
     }
 
