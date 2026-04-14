a55752e65a41b368dad3155105b51208dd8e080a
luck-jay <zhouchanghong@xiaomi.com>
2026-04-14 20:42:14 +0800

    aoligei

--- src/leecode_exer/AA_A运行脚本.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM ==========================
-REM BAT launcher (NO CHINESE)
-REM ==========================
-
-REM Run pw.ps1 in current folder
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0AA_pw.ps1"
-
-pause
